abe3ebb98b5176f2d5be6b783f7b773e73f698dc
Gaurav Bhardwaj <96366072+BhardwajG572@users.noreply.github.com>
2026-08-02 13:22:11 +0530

    Create stored procedure to load data into bronze schema
    This stored procedure loads data into the 'bronze' schema from external CSV files, truncating the bronze tables before loading data using the BULK INSERT command.

--- scripts/bronze/proc_load_bronze.sql
+++ scripts/bronze/proc_load_bronze.sql
@@ -1,1 +1,71 @@
 
+/*
+
+=======================================================================
+
+Stored Procedure : Load Bronze Layer (Source --> Bronze) 
+
+======================================================================
+
+Script Purpose:
+	This stored procedure loads data into the 'bronze' schema from external csv files.
+	It performs the following actions.
+	- Truncates the bronze tables before loading data.
+    - Uses the `BULK INSERT` command to load data from csv Files to bronze tables.
+
+Parameters:
+	None.
+		This stored procedure does not accept any parameters or return any values.
+
+Usuage Example:
+	EXEC bronze.load_bronze
+
+
+
+*/
+
+EXEC bronze.load_bronze;
+
+
+CREATE or ALTER PROCEDURE bronze.load_bronze AS 
+BEGIN
+	DECLARE @start_time DATETIME , @end_time DATETIME
+	BEGIN TRY
+	
+
+		PRINT '================================================';
+		PRINT 'Loading Bronze Layer';
+		PRINT '================================================';
+
+		set @start_time = GETDATE(); 
+
+		PRINT '>> Truncating Table: bronze.marketing_data'
+
+		TRUNCATE TABLE bronze.marketing_data;
+
+		PRINT '>> Inserting Data Into : Bronze.marketing_data'
+		
+		BULK INSERT bronze.marketing_data
+
+		FROM 'C:\Users\.m\Downloads\Marketing_Data_Maven_Marketing_challenges\marketing_data.csv'
+
+		WITH (
+			FIRSTROW = 2,
+			FIELDTERMINATOR = ',',
+			TABLOCK
+		);
+
+		set @end_time = GETDATE() ; 
+
+		PRINT '>> Load Duration: '  + CAST(DATEDIFF(second , @start_time , @end_time) AS NVARCHAR) + ' seconds' ;
+        PRINT '>> ------------------------------------------------------------';
+
+    END TRY
+    BEGIN CATCH
+        PRINT '================================================';
+        PRINT 'ERROR OCCURRED DURING LOAD';
+        PRINT 'Error Message: ' + ERROR_MESSAGE();
+        PRINT '================================================';
+    END CATCH
+END;
+GO
